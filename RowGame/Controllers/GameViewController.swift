eea2aceac78c7c1c17d5819f00005359306bb16a
Noccis <toni.lof@iths.se>
2021-11-25 06:34:37 -0800

    GameBoard working, playernr adds and image

--- RowGame/Controllers/GameViewController.swift
+++ RowGame/Controllers/GameViewController.swift
@@ -23,6 +23,21 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var imageOne: UIImageView!
     
+    @IBOutlet weak var imageTwo: UIImageView!
+    
+    @IBOutlet weak var imageThree: UIImageView!
+    
+    @IBOutlet weak var imageFour: UIImageView!
+    
+    @IBOutlet weak var imageFive: UIImageView!
+    
+    @IBOutlet weak var imageSix: UIImageView!
+    
+    @IBOutlet weak var imageSeven: UIImageView!
+    
+    @IBOutlet weak var imageEight: UIImageView!
+    
+    
     
     
     
@@ -50,6 +65,86 @@ class GameViewController: UIViewController {
     }
     
     
+    @IBAction func tapTwo(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 2
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageTwo)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    
+    @IBAction func tapThree(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 3
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageThree)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    @IBAction func tapFour(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 4
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageFour)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    
+    @IBAction func tapFive(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 5
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageFive)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    
+    @IBAction func tapSix(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 6
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageSix)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    @IBAction func tapSeven(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 7
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageSeven)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    
+    @IBAction func tapEight(_ sender: UITapGestureRecognizer) {
+        let arrayNr = 8
+        if gameBoard.boardArray[arrayNr] == 0 {
+            setPlayerMark(arrayNr: arrayNr, imageOutlet: imageEight)
+          
+        }else{
+            print("Invalid move!")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -63,15 +158,16 @@ class GameViewController: UIViewController {
             gameBoard.boardArray[arrayNr] = 1
             imageOutlet.image = UIImage(named: "xgreen")
             playerActive.toggle()
-            print("Set player mark func running, P1")
+            print("Set player mark func running, P1. Number \(gameBoard.boardArray[arrayNr]) set to mark")
+            print(gameBoard.boardArray)
             
         }else {
             // Player two active
             gameBoard.boardArray[arrayNr] = 2
             imageOutlet.image = UIImage(named: "cirkle")
             playerActive.toggle()
-            print("Set player mark func running, P2")
-            
+            print("Set player mark func running, P2 Number \(gameBoard.boardArray[arrayNr]) set to mark")
+            print(gameBoard.boardArray)
         }
         
     }
