b6d94c68ad6bee1a118606f970c7a3ef3346dfe6
Toni <89582846+Noccis@users.noreply.github.com>
2021-12-09 04:28:39 -0800

    added check that name is not empty

--- RowGame/Controllers/ViewController.swift
+++ RowGame/Controllers/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  
+    var robotPlayer = false     // Bool to deside if there should be 2 string inputs.
+    
+    
     
     @IBOutlet weak var welcomeLabel: UILabel!
     
-
+    
     @IBOutlet weak var enterNameLabel1: UILabel!
     
     @IBOutlet weak var enterNameLabel2: UILabel!
@@ -36,11 +38,11 @@ class ViewController: UIViewController {
         startGameButton.isHidden = true
         
         welcomeLabel.text = "Welcome to Row Game! \n Who do you want to play against?"
-       
+        
         
     }
     
-
+    
     @IBAction func playWithFriend(_ sender: UIButton) {
         
         enterNameLabel1.isHidden = false
@@ -50,6 +52,8 @@ class ViewController: UIViewController {
         startGameButton.isHidden = false
         
         userInputText1.becomeFirstResponder()
+        
+        robotPlayer = false
         
     }
     
@@ -62,23 +66,51 @@ class ViewController: UIViewController {
         
         userInputText1.becomeFirstResponder()
         
+        robotPlayer = true
+        
     }
     
     
-   
+    @IBAction func playGame(_ sender: UIButton) {
+        
+        if robotPlayer {    // If player 2 will be a robot.
+            
+            if userInputText1.text == "" {
+                return
+            }
+                performSegue(withIdentifier: "gameSegue", sender: self)
+            
+            
+        }else{  // If player 2 is human.
+            
+            if userInputText1.text == "" {  // Check that name string is not empty
+                return
+            }
+            if userInputText2.text == "" {
+                return
+            }
+            
+        
+            performSegue(withIdentifier: "gameSegue", sender: self)
+            
+        }
+        
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "gameSegue" {
             
             guard let destinationVC = segue.destination as? GameViewController else {return}
+            
             destinationVC.p1Name = userInputText1.text
             destinationVC.p2Name = userInputText2.text
             
             
         }
-           
-       }
+        
+    }
     
 }
 
